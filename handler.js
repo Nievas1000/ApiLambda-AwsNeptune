e1a56776a55844e8a13205b2d048f74a3d820f30
Lautaro Nievas <lautynievas09@gmail.com>
2022-12-13 11:22:13 -0300

    CI with lint, snyk and deploy

--- handler.js
+++ handler.js
@@ -1,18 +1,25 @@
-'use strict';
+const gremlin = require('gremlin');
+require('dotenv').config();
+// functions / definitions from gremlin js library
+const DriverRemoteConnection = gremlin.driver.DriverRemoteConnection;
+const Graph = gremlin.structure.Graph;
+function getNeptuneWSURL(neptuneHost, neptunePort) {
+	const url = 'wss://' + neptuneHost + ':' + neptunePort + '/gremlin';
+	return url;
+}
+// Create connection to Neptune
+const dc = new DriverRemoteConnection(
+	getNeptuneWSURL(process.env.DATABASE_INSTANCE, process.env.DATABASE_PORT),
+	{}
+);
+const graph = new Graph();
+const g = graph.traversal().withRemote(dc);
 
 module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+	// await g.addV("person").property("name", "stephen").next();
+	const result = await g.V().values('name').toList();
+	return {
+		statusCode: 200,
+		body: JSON.stringify({ message: 'Testing Gremlin!', data: result }),
+	};
 };
